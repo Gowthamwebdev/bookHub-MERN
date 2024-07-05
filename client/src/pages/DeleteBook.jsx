d1b7d35179b0b488199343e2bd50fe0fbe85b90e
Gowtham <137925188+Gowthamwebdev@users.noreply.github.com>
2024-07-05 22:47:47 +0530

    Deletebook logic file created

--- client/src/pages/DeleteBook.jsx
+++ client/src/pages/DeleteBook.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BackButton from "../components/BackButton";
+import { useParams } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const DeleteBook = () => {
-  return <div>deleteBook</div>;
+  const { id } = use.params();
+  const handleDelete = () => {
+    axios.delete("http://localhost:5000:books/").then()
+  };
+  return (
+    <div className="flex items-center justify-center bg-transparent blur-md">
+      <button className="bg-teal-500 w-36 h-12" onClick={handleDelete}></button>
+    </div>
+  );
 };
 
 export default DeleteBook;
